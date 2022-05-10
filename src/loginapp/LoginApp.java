ed63e4f2808d80950afeb9965d1e616325064591
Ernesto Louis <elouis12@me.com>
2022-05-09 21:45:24 -0400

    did major debugging as to why JavaFX wasn't working

---
+++ src/loginapp/LoginApp.java
@@ -0,0 +1,20 @@
+package loginapp;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class LoginApp extends Application { // so we can create an application
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+
+
+    }
+
+    public static void main(String[] args) {
+
+        launch( args );
+    }
+}
